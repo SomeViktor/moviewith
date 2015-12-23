39cda7c458657ceb6f9dfc3bc13675e65a8cb230
Viktor <mail@viktor-schlueter.de>
2015-12-23 10:24:46 -0500

    stuff works better

---
+++ frontend/js/script.js
@@ -0,0 +1,80 @@
+var j= '[["John Travolta", "Samuel L. Jackson"],["Kristen Stewart", "Robert Pattison"],["somebody", "bla"],["nana", "mamama"]]';
+var i = 1;
+var jobject = JSON.parse(j);
+
+
+setInterval(updateNames, 3000);
+
+var i = 0;
+function updateNames (){
+
+	document.getElementById('a1').innerHTML = jobject[i][0];
+	document.getElementById('a2').innerHTML = jobject[i][1];
+	i++;
+	if(i==4){
+		i=0;
+	}
+
+
+}
+
+$( document ).ready(function() {
+    
+	
+	$('#in1').on('input', function(event){
+		ask_for_suggestions(1,$('#in1').val() );
+    });
+    $('#in2').on('input', function(event){
+        ask_for_suggestions(2,$('#in2').val() );
+    });
+    $('#in3').on('input', function(event){
+        ask_for_suggestions(3,$('#in3').val() );
+    });
+});
+
+
+
+
+
+function ask_for_suggestions(input_nr, content){
+	console.log("aks for suggestion:");
+	console.log(i);
+	console.log(content);
+    var exec_url = "/suggestions";
+    var post_data = {};
+    post_data["content"] = content;
+    $.ajax({
+        type: "POST",
+        url: exec_url,
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(post_data),
+        success:function(data){
+            var jsonthing = JSON.parse(data);
+            console.log(jsonthing);
+            
+        }
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ moviesapp/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile("public/index.html")
+});
+
+
+router.get('/suggestions', function(req, res, next) {
+  console.log("got request");
+  var response =[];
+  response.push("John Travolta");
+  response.push("John Travolta");
+  res.json(response);
+});
+
+
+module.exports = router;
